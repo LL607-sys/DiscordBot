1b55ba6f4f152577cd5a49cdece43f588c6a6816
LL607_ <77607428+LL607-sys@users.noreply.github.com>
2021-02-22 10:38:31 +0100

    Delete help.js

--- help.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    help: async function(client){ 
-        client.on('message', msg => {
-            if (msg.content.startsWith("!!help")) {
-                msg.channel.send("```\n!!help = alle cmds\n!!kick (@user) = kickt den user (admin only)\n!!log = schickt den log (admin only)\n!!ticket = erstellt eine msg für channel createn (admin only)```")
-            }
-        })
-    }}
