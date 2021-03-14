922b42f9f84c7a2fca7bc00637be4d30b99764ed
LL607_ <77607428+LL607-sys@users.noreply.github.com>
2021-03-14 12:50:52 +0100

    More commands

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Discord = require('discord.js');
 const kick = require('./cmd/kick.js');
 const client = new Discord.Client();
+const fs = require ('fs');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('LL607.me');
 });
   const log = require("./cmd/loghandler.js")
   const kack = require("./cmd/kick.js")
   const help = require("./cmd/help.js")
   const logsend = require("./cmd/log.js")
   const ticket = require("./cmd/ticket.js")
+  const ban = require("./cmd/ban.js")
 
       log.joinguild(client)
       log.leaveguild(client)
@@ -25,4 +28,7 @@ client.on('ready', () => {
       logsend.Penis(client)
 
       ticket.ticket(client)
+
+      ban.ban(client)
 client.login('Token');
+console.log(".____    .____     ________________________    \n|    |   |    |   /  _____/\\   _  \\______  \\   \n|    |   |    |  /   __  \\ /  /_\\  \\  /    /   \n|    |___|    |__\\  |__\\  \\\\  \\_/   \\/    /    \n|_______ \\_______ \\_____  / \\_____  /____/_____\n        \\/       \\/     \\/        \\/    /_____/")
