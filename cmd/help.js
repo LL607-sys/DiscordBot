805d5ece8f79cd723a54c6a02cbbc52bf9058f21
LL607_ <77607428+LL607-sys@users.noreply.github.com>
2021-03-14 12:51:31 +0100

    Now Help in Embed

--- cmd/help.js
+++ cmd/help.js
@@ -1,8 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 module.exports = {
     help: async function(client){ 
-        client.on('message', msg => {
-            if (msg.content.startsWith("!!help")) {
-                msg.channel.send("```\n!!help = alle cmds\n!!kick (@user) = kickt den user (admin only)\n!!log = schickt den log (admin only)\n!!ticket = erstellt eine msg für channel createn (admin only)```")
+        client.on('message', message => {
+            if (message.content.startsWith("!!help")) {
+                try{
+                const embed = new Discord.MessageEmbed()
+                .setTitle('Commands:')
+                .setColor(000000)
+                .setDescription('**!Attention!**\n\n**Prefix is !!**\n\n``!!help``\n_all cmds_\n\n``!!kick (@user)``\n_kicks the user_\n\n``!!ban (@user)``\n_bans the user_\n\n``!!log``\n_sends the log_\n\n``!!ticket``\n_creates a embed Message_');
+                message.channel.send(embed)
+                } catch (e) {console.error("Failed to create embed")}
             }
         })
     }}
