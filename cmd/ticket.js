c87153018f1eaae240d37491e4063109ee1b7126
LL607_ <77607428+LL607-sys@users.noreply.github.com>
2021-03-14 12:54:18 +0100

    Bug fixes

--- cmd/ticket.js
+++ cmd/ticket.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require('fs');
+var fb = []
 
 module.exports = {
   ticket: async function(client){ 
@@ -15,34 +17,85 @@ client.on('message', message => {
         message.channel.send(embed).then(sentEmbed => {
         sentEmbed.react("📄")
       })
-      
-    }catch (e) {console.error('Failed to remove reactions.');} 
+      //creates the embed message
+    }catch (e) {console.error('Failed to create embed message.');} 
   }; 
 });
 
-client.on('messageReactionAdd', (reaction, user) => {
-  var message = reaction.message
-  if(!user.bot) {
-    try{
-      var userid = user.id
-      var names = user.tag.substring(0, user.tag.length - 5);
-      var everyoneRole = message.guild.roles.everyone.id;
-      message.guild.channels.create(names, {
-        type: "text",
-         parent: "811643384770986005",
-          permissionOverwrites: [
-              {
-                 id: userid,
-                 allow: ['VIEW_CHANNEL'],
-              },
-              {
-                id: everyoneRole,
-                deny: ['VIEW_CHANNEL']
-              }
-            ]
-      });
-        reaction.users.remove(userid);
-    }catch (e) {console.error('Failed to create the Channel');}
+client.on("messageReactionAdd", (reaction, user) => {
+    let message = reaction.message, emoji = reaction.emoji;
+    fs.readFile("activeTickets.txt", async function (err, data) {
+      if (err) throw err;
+      //opens the activeTickets.txt file
+        if (emoji.name == '📄') {
+          if (!user.bot) {
+          var userid = user.id;
+          var names = user.tag.substring(0, user.tag.length - 5);
+          var everyoneRole = message.guild.roles.everyone.id;
+          if (data.includes(names)) {
+            message.channel
+              .send(names + "Du hast schon einen channel")
+              .then((msg) => {
+                msg.delete({ timeout: 10000 });
+              });
+            reaction.users.remove(userid);
+            //checks if the user has already an channel
+    
+          } else {
+            var names = user.tag.substring(0, user.tag.length - 5);
+            //if he has not then it creates an new channel
+            message.guild.channels
+              .create(names, {
+                type: "text",
+                parent: "811643384770986005",
+                permissionOverwrites: [
+                  {
+                    id: userid,
+                    allow: ["VIEW_CHANNEL"],
+                  },
+                  {
+                    id: everyoneRole,
+                    deny: ["VIEW_CHANNEL"],
+                  },
+                ],
+              })
+              .then((cchannel) => {
+                fb.push(cchannel.id.toString())
+                reaction.users.remove(userid);
+                fs.appendFile(
+                  "activeTickets.txt",
+                  names + "\n",
+                  function (err) {
+                    if (err) return console.log(err);
+                    console.log("Saved");
+                  }
+                );
+                //logs that this user now has an channel
+                const embed = new Discord.MessageEmbed()
+                  .setTitle("Support Ticket")
+                  .setColor(0xff0000)
+                  .setDescription(
+                    "If you want to close this Channel.\n React with 🔒 to close."
+                  );
+                cchannel.send(embed).then((sentEmbed) => {
+                  sentEmbed.react("🔒");
+                });
+              });
+          }
+      }}
+    });
+  if (emoji.name == '🔒') {
+    fs.readFile("activeTickets.txt", async function (err, data) {
+    if(!user.bot) {
+      if((fb.indexOf(message.channel.id.toString()) > -1)){
+          var names = user.tag.substring(0, user.tag.length - 5);
+          var replace = "."
+          const fetchchannel = message.channel;
+          fetchchannel.delete();
+          fs.writeFileSync("activeTickets.txt", replace);
+      }
   }
-})
+  }
+    )}
+});
 }}
